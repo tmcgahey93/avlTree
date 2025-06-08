17475241db22b42db5b6c1e4bf575acc540eedfa
tmcgahey93 <tmcgahey93@gmail.com>
2025-06-08 09:21:52 -0400

    Adding Test File

---
+++ avlTree_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	//got := Add(2, 3)
+	//want := 5
+
+	//if got != want {
+	//    t.Errorf("Add(2, 3) = %d; want %d", got, want)
+	//}
+}
